b3d6b52834aad74ac44f20e1dda51e088292c479
Govind Choudhary <101502681+GovindChoudhary844@users.noreply.github.com>
2025-06-26 13:15:12 +0530

    first commit

---
+++ src/Components/Projects/ProjectOdd.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Row, Col, Nav } from "react-bootstrap";
+import projectsAPI from "../api/projectsAPI";
+import "../../App.css";
+
+function ProjectOdd({ index }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const navigate = useNavigate();
+
+  const project = projectsAPI[index];
+
+  // Guard clause to handle undefined project
+  if (!project) {
+    return <div>Error: Project data not found.</div>;
+  }
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  const redirectToProjectDetails = () => {
+    navigate(`/projects/${project.id}`);
+  };
+
+  return (
+    <div>
+      <style>
+        {`
+          .italic {
+            font-style: italic;
+          }
+          .image-container {
+            position: relative;
+            overflow: hidden;
+            cursor: pointer;
+          }
+          .image-size {
+            transition: transform 0.5s ease, opacity 0.5s ease;
+            display: block;
+            width: 100%;
+            height: auto;
+          }
+          .image-container:hover .image-size {
+            transform: scale(1.1);
+            opacity: 0.5;
+          }
+          .play-icon {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 3rem;
+            color: white;
+            opacity: 0;
+            transition: opacity 0.5s ease;
+          }
+          .image-container:hover .play-icon {
+            opacity: 1;
+          }
+          .blur {
+            filter: blur(10px);
+          }
+        `}
+      </style>
+      <Row className="d-flex align-items-center">
+        <Col md={7}>
+          <Nav.Link as={Link} to={`/projects/${project.id}`}>
+            <h5 className="resp-h5 heading">
+              {project.name} ({project.date}){" "}
+              <i className="fa-regular fa-arrow-up-right-from-square fa-sm"></i>
+            </h5>
+          </Nav.Link>
+          <p
+            className="italic resp-text"
+            style={{
+              color: "var(--sixth-color)",
+            }}
+          >
+            {project.subname}
+          </p>
+          <p className="resp-text">{project.description}</p>
+        </Col>
+        <Col md={5}>
+          <div className="image-container" onClick={redirectToProjectDetails}>
+            <img
+              src={process.env.PUBLIC_URL + "/" + project.imagecharacter}
+              alt={project.name}
+              className={`image-size ${imageLoaded ? "" : "blur"}`}
+              loading="lazy"
+              onLoad={handleImageLoad}
+            />
+            <i className="fa fa-play-circle play-icon" aria-hidden="true"></i>
+          </div>
+        </Col>
+        <hr className="hr-small my-lg-3" />
+      </Row>
+    </div>
+  );
+}
+
+export default ProjectOdd;
---
+++ src/Components/api/projectsAPI.js
@@ -0,0 +1,221 @@
+const projectsAPI = [
+  {
+    id: 1,
+    name: "Hero Hop",
+    subname: "2D Platformer Game",
+    GitHub_Link: "https://github.com/GovindChoudhary844/Hero_Hop",
+    Game_Link: "https://govindchoudhary844.github.io/HeroHop_Game_URL/",
+    description: (
+      <>
+        <div className="resp-h2">HeroHop</div>
+        <br />
+        ▪ Exciting 2D platformer where you play as a brave dragon with unique
+        abilities.
+        <br />
+        ▪ Navigate through challenging levels showcasing skills like movement,
+        jumps, double jumps, wall walks, and fireball attacks.
+        <br />
+        ▪ Face dangerous traps such as arrows, fire, and a menacing spike ball
+        that hunts you down.
+        <br />
+        ▪ Outsmart a formidable knight enemy guarding a secret location.
+        <br />
+        ▪ Collect health items to stay in the fight and conquer two thrilling
+        levels.
+        <br />
+        ▪ Take on the challenge and save the day in this adventure-filled game!
+        <br />
+      </>
+    ),
+
+    imagecharacter: "Images/Projects/HeroHop/6.png",
+    imagesDesktop: [
+      "Images/Projects/HeroHop/1.png",
+      "Images/Projects/HeroHop/2.png",
+      "Images/Projects/HeroHop/3.png",
+      "Images/Projects/HeroHop/4.png",
+      "Images/Projects/HeroHop/5.png",
+      "Images/Projects/HeroHop/6.png",
+    ],
+  },
+  {
+    id: 2,
+    name: "Survive The Shadows",
+    subname: "2D Platformer Game",
+    GitHub_Link: "https://github.com/GovindChoudhary844/Survive_the_shadows",
+    Game_Link: "https://govindchoudhary844.github.io/Survive_the_shadows_URL/",
+    description: (
+      <>
+        <div className="resp-h2">Survive the Shadows</div>
+        <br />
+        ▪ Action-packed 2D platformer featuring a hero with a sword, capable of
+        3 unique attack moves, a jump attack, and arrow throwing.
+        <br />
+        ▪ Battle against a vigilant knight who patrols and detects walls and
+        edges, and a FlyingEye that attacks when in range.
+        <br />
+        ▪ Collect health pickups to stay in the fight and enhance your survival
+        chances.
+        <br />
+        ▪ Smooth camera transitions powered by Cinemachine and responsive
+        controls using Unity's Input System.
+        <br />
+        ▪ Beautifully designed levels using the 2D Tilemap Editor, offering a
+        visually engaging experience.
+        <br />
+        ▪ Thrilling and immersive gameplay that challenges your skills and
+        reflexes.
+        <br />
+      </>
+    ),
+    imagecharacter: "Images/Projects/Survive_the_shadows/Profile.png",
+    imagesDesktop: [
+      "Images/Projects/Survive_the_shadows/1.png",
+      "Images/Projects/Survive_the_shadows/2.png",
+      "Images/Projects/Survive_the_shadows/3.png",
+      "Images/Projects/Survive_the_shadows/4.png",
+      "Images/Projects/Survive_the_shadows/5.png",
+      "Images/Projects/Survive_the_shadows/6.png",
+      "Images/Projects/Survive_the_shadows/7.png",
+      "Images/Projects/Survive_the_shadows/8.png",
+      "Images/Projects/Survive_the_shadows/9.png",
+    ],
+  },
+  {
+    id: 3,
+    name: "2D Top-Down RPG Game",
+    subname: "2D Top-Down RPG",
+    GitHub_Link: "https://github.com/GovindChoudhary844/2D-Top-Down-RPG",
+    Game_Link: "https://govindchoudhary844.github.io/2D-Top-Down-RPG-URL/",
+    description: (
+      <>
+        <div className="resp-h2">2D Top-Down RPG Game</div>
+        <br />
+        <br />
+        ▪ Implements a player input system for smooth and responsive controls.
+        <br />
+        ▪ Features dynamic animations for player and enemy actions, including
+        dashing and knockback.
+        <br />
+        ▪ Includes a modular weapon system with melee, ranged attacks, and
+        cooldown management.
+        <br />
+        ▪ Provides interactive environments with destructible objects, rule
+        tiles, and portal effects.
+        <br />
+        ▪ Enhances gameplay with 2D lighting, fade transitions, and camera
+        control using Cinemachine.
+        <br />
+        ▪ Supports multiple scenes with seamless transitions and immersive
+        effects.
+        <br />
+        ▪ Designed a user-friendly UI with toggle options and transparency
+        settings for better gameplay experience.
+        <br />
+      </>
+    ),
+    imagecharacter: "Images/Projects/2D_Top_Down_RPG/1.png",
+    imagesDesktop: [
+      "Images/Projects/2D_Top_Down_RPG/1.png",
+      "Images/Projects/2D_Top_Down_RPG/2.png",
+      "Images/Projects/2D_Top_Down_RPG/3.png",
+      "Images/Projects/2D_Top_Down_RPG/4.png",
+      "Images/Projects/2D_Top_Down_RPG/5.png",
+      "Images/Projects/2D_Top_Down_RPG/6.png",
+      "Images/Projects/2D_Top_Down_RPG/7.png",
+    ],
+  },
+  {
+    id: 4,
+    name: "Ballz Game",
+    subname: "Blow all the Bubbles with Balls",
+    GitHub_Link: "https://github.com/GovindChoudhary844/Ballz",
+    Game_Link: "https://govindchoudhary844.github.io/Ballz_URL/",
+    description: (
+      <>
+        <div className="resp-h2">Ballz Game</div>
+        <br />
+        1. This is a 2D game where the player controls a gun that can shoot
+        multiple balls.
+        <br />
+        2. Bubbles appear on the screen, each with a random number between 1 and
+        9.
+        <br />
+        3. The balls bounce off the bubbles, and each hit reduces the bubble's
+        number.
+        <br />
+        4. When a bubble’s number reaches 0, a pop animation is played and the
+        bubble gets destroyed.
+        <br />
+        5. The game ends once all the bubbles are destroyed.
+      </>
+    ),
+    imagecharacter: "Images/Projects/Ballz/Front.png",
+    imagesDesktop: [
+      "Images/Projects/Ballz/Front.png",
+      "Images/Projects/Ballz/Front.png",
+      "Images/Projects/Ballz/Front.png",
+      "Images/Projects/Ballz/Front.png",
+    ],
+  },
+  {
+    id: 5,
+    name: "Ludo Game",
+    subname: "Ludo With AI Player",
+    GitHub_Link: "https://github.com/GovindChoudhary844/Ludo",
+    Game_Link: "https://govindchoudhary844.github.io/Ludo_URL/",
+    description: (
+      <>
+        <div className="resp-h2">Ludo Game</div>
+        <br />
+        1. Developed a Unity 2D Ludo game with player vs AI mode.
+        <br />
+        2. Added a menu to choose the number of players (2, 3, or 4).
+        <br />
+        3. Included options to select the number of gotis (1, 2, 3, or 4).
+        <br />
+        4. Designed a game-over screen that shows the players' total scores.
+        <br />
+        5. Focused on user-friendly design and smooth gameplay.
+      </>
+    ),
+    imagecharacter: "Images/Projects/Ludo/Cover.png",
+    imagesDesktop: [
+      "Images/Projects/Ludo/Cover.png",
+      "Images/Projects/Ludo/Cover.png",
+      "Images/Projects/Ludo/Cover.png",
+      "Images/Projects/Ludo/Cover.png",
+    ],
+  },
+  {
+    id: 6,
+    name: "Zombie TPS Game",
+    subname: "Open World Game",
+    GitHub_Link: "https://github.com/GovindChoudhary844/Ludo",
+    Game_Link: "https://www.youtube.com/embed/f4kF9cBEGow",
+    description: (
+      <>
+        <div className="resp-h2">Zombie TPS</div>
+        <br />
+        1. Developed a Unity 2D Ludo game with player vs AI mode.
+        <br />
+        2. Added a menu to choose the number of players (2, 3, or 4).
+        <br />
+        3. Included options to select the number of gotis (1, 2, 3, or 4).
+        <br />
+        4. Designed a game-over screen that shows the players' total scores.
+        <br />
+        5. Focused on user-friendly design and smooth gameplay.
+      </>
+    ),
+    imagecharacter: "Images/Projects/Zombie TPS/Zombie Wallpapers.jpg",
+    imagesDesktop: [
+      "Images/Projects/Zombie TPS/Zombie Wallpapers.jpg",
+      "Images/Projects/Zombie TPS/Zombie Wallpapers.jpg",
+      "Images/Projects/Zombie TPS/Zombie Wallpapers.jpg",
+      "Images/Projects/Zombie TPS/Zombie Wallpapers.jpg",
+    ],
+  },
+];
+
+export default projectsAPI;
